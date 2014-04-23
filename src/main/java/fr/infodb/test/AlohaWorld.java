a2b4234a55a1383ef798932b56b2523a18c75b58
Nomade Lampe <nomadelampe@gmail.com>
2014-04-23 11:08:28 +0200

    Ajout du fichier *.iml dans le .gitignore

--- src/main/java/fr/infodb/test/AlohaWorld.java
+++ src/main/java/fr/infodb/test/AlohaWorld.java
@@ -4,7 +4,8 @@ public class AlohaWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Aloha !ddfsdfs!!!");
+		System.out.println("Aloha !");
+		System.out.println("Bonjour !");
 
 	}
 
